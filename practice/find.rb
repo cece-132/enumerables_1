b8899026781250918caa3e1aea1b856482bffbca
Candace Eckels <candace.eckels@gmail.com>
2023-02-16 23:35:20 -0700

    find practice complete

--- practice/find.rb
+++ practice/find.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # All of the methods below have been started for you. To test your code, uncomment the line at the bottom of the page that 
 # prints out the return value of that method.  
 
@@ -14,10 +15,8 @@ end
 
 def no_waldo
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
-    found = words.find do |word|
-
-      # Your code goes here
-
+    words.find do |word|
+      word == "waldo"
     end
 
     # expected return value is nil
@@ -26,7 +25,9 @@ end
 def find_waldo
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
 
-    # Your code goes here
+    words.find do |word|
+        word == "waldo"
+    end
 
     # expected return value is "waldo"
 end
@@ -34,7 +35,9 @@ end
 def cannot_find_3_letter_words
     words = ["piglet", "porridge", "bear", "blueberry"]
 
-    # Your code goes here
+    words.find do |word|
+        word.length == 3
+    end
 
     # expected return value is nil
 end
@@ -42,7 +45,9 @@ end
 def find_13
     numbers = [2, 13, 19, 8, 3, 27]
 
-    # Your code goes here
+    numbers.find do |num|
+        num == 13
+    end
 
     # expected return value is 13
 end
@@ -50,7 +55,9 @@ end
 def find_first_even_number
     numbers = [3, 7, 13, 11, 10, 2, 17]
 
-    # Your code goes here
+    numbers.find do |num|
+        num.even?
+    end
 
     # expected return value is 10
 end
@@ -58,7 +65,9 @@ end
 def find_first_multiple_of_3
     numbers = [2, 8, 9, 27, 24, 5]
 
-    # Your code goes here
+    numbers.find do |num|
+        num % 3 == 0
+    end
 
     # expected return value is 9
 end
@@ -66,7 +75,9 @@ end
 def find_first_word_starting_with_q
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
 
-    # Your code goes here
+    words.find do |word|
+        word[0] == 'q'
+    end
 
     # expected return value is "quill"
 end
@@ -74,7 +85,9 @@ end
 def find_first_word_ending_with_er
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
 
-    # Your code goes here
+    words.find do |word|
+        word[-2..-1] == 'er'
+    end
 
     # expected return value is "finger"
 end
@@ -82,18 +95,20 @@ end
 def find_first_number_greater_than_20
     numbers = [1, 8, 19, 21, 29, 31, 34]
 
-    # Your code goes here
+    numbers.find do |num|
+        num > 20
+    end
 
     # expected return value is 21
 end
 
-# p find_first_seven_letter_word
-# p no_waldo
-# p find_waldo
-# p cannot_find_3_letter_words
-# p find_13
-# p find_first_even_number
-# p find_first_multiple_of_3
-# p find_first_word_starting_with_q
-# p find_first_word_ending_with_er
-# p find_first_number_greater_than_20
+p find_first_seven_letter_word
+p no_waldo
+p find_waldo
+p cannot_find_3_letter_words
+p find_13
+p find_first_even_number
+p find_first_multiple_of_3
+p find_first_word_starting_with_q
+p find_first_word_ending_with_er
+p find_first_number_greater_than_20
